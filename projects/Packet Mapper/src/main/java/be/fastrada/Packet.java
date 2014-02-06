5fc97f545502ef2001d6fbfcdaf6832ca2ac029e
thebillkidy <thebillkidy@gmail.com>
2014-02-06 16:47:38 +0100

    Basic Packet reader has been fixed

--- projects/Packet Mapper/src/main/java/be/fastrada/Packet.java
+++ projects/Packet Mapper/src/main/java/be/fastrada/Packet.java
@@ -4,33 +4,53 @@ import java.math.BigInteger;
 
 public class Packet {
 
-    private String hexContent;
+    private String content;
+    private int position;
 
     public Packet(String packetString) {
-        hexContent = packetString.replace(" ", "");
+        this.content = packetString.replace(" ", "");
+        this.position = 0;
     }
 
     @Override
     public String toString() {
-        return hexContent;
+        return content;
     }
 
-    public BigInteger toBigInt() {
-        return new BigInteger(hexContent, 16);
+    public String getContent() {
+        return content;
     }
 
-    public BigInteger applyMask(BigInteger mask) {
-        BigInteger tempResult = toBigInt();
-        BigInteger result = tempResult.and(mask);
-        return result.shiftRight(32);
+    public int getPosition() {
+        return this.position;
     }
 
-    public int readId() {
-
-        return 0;
+    public long readUint8() {
+        int byteHexLength = 2;
+        long result = Long.parseLong(content.substring(position, position + byteHexLength), 16);
+        position += byteHexLength;
+        return result;
     }
 
-    public String getHexString() {
-        return hexContent;
+    public long readUint16() {
+        int byteHexLength = 4;
+        long result = Long.parseLong(content.substring(position, position + byteHexLength), 16);
+        position += byteHexLength;
+        return result;
+    }
+
+    public long readUint32() {
+        int byteHexLength = 8;
+        long result = Long.parseLong(content.substring(position, position + byteHexLength), 16);
+        position += byteHexLength;
+        return result;
+    }
+
+    public void resetPosition() {
+        this.position = 0;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
--- projects/Packet Mapper/src/test/java/be/fastrada/PacketTests.java
+++ projects/Packet Mapper/src/test/java/be/fastrada/PacketTests.java
@@ -8,20 +8,93 @@ import static junit.framework.Assert.assertEquals;
 
 public class PacketTests {
 
-    private Packet packet = new Packet("00 FF FF 0A 17 00 00 00 00");
+    private Packet packet = new Packet("01 FF FF 0A 17 00 00 00 00");
 
     @Test
-    public void createPacket() {
-        assertEquals(packet.getHexString(), "00FFFF0A1700000000");
+    public void packetContent() {
+        assertEquals(packet.getContent(), "01FFFF0A1700000000");
     }
 
     @Test
-    public void packetToLong() {
-        assertEquals(packet.toBigInt(), new BigInteger("18446473692633366528"));
+    public void packetToString() {
+        assertEquals(packet.toString(), "01FFFF0A1700000000");
     }
 
     @Test
-    public void readId() {
-        assertEquals(packet.readId(), 0);
+    public void changePositionBy1ByteAndReset() {
+        // Test reading a byte as test
+        assertEquals(0, packet.getPosition());
+        assertEquals(1, packet.readUint8());
+        assertEquals(2, packet.getPosition());
+
+        // Reset to 0 position
+        packet.resetPosition();
+        assertEquals(0, packet.getPosition());
+
     }
+
+    @Test
+    public void readByte() {
+        assertEquals(0, packet.getPosition());
+        assertEquals(1, packet.readUint8());
+        assertEquals(2, packet.getPosition());
+    }
+
+    @Test
+    public void readMaxByte() {
+        packet.setContent("FF");
+        assertEquals(0, packet.getPosition());
+        assertEquals(255, packet.readUint8());
+        assertEquals(2, packet.getPosition());
+    }
+
+    @Test
+    public void readShort() {
+        assertEquals(0, packet.getPosition());
+        assertEquals(511, packet.readUint16()); // 01FF
+        assertEquals(4, packet.getPosition());
+    }
+
+    @Test
+    public void readMaxShort() {
+        packet.setContent("FFFF");
+        assertEquals(0, packet.getPosition());
+        assertEquals(65535, packet.readUint16());
+        assertEquals(4, packet.getPosition());
+    }
+
+    @Test
+    public void readInt() {
+        assertEquals(0, packet.getPosition());
+        assertEquals(33554186, packet.readUint32());
+        assertEquals(8, packet.getPosition());
+    }
+
+    @Test
+    public void readMaxInt() {
+        packet.setContent("FFFFFFFF");
+        assertEquals(0, packet.getPosition());
+        assertEquals(4294967295L, packet.readUint32());
+        assertEquals(8, packet.getPosition());
+    }
+
+    @Test
+    public void testByteAndShortAndInt() {
+        // Read Byte
+        assertEquals(0, packet.getPosition());
+        assertEquals(1, packet.readUint8());
+        assertEquals(2, packet.getPosition());
+
+        // Read Short
+        assertEquals(2, packet.getPosition());
+        assertEquals(65535, packet.readUint16());
+        assertEquals(6, packet.getPosition());
+
+        // Read Int
+        assertEquals(6, packet.getPosition());
+        assertEquals(169279488, packet.readUint32());
+        assertEquals(14, packet.getPosition());
+    }
+
+    // Test if we reached end of the hex!
 }
