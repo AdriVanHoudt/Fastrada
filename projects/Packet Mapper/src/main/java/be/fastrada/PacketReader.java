c0bb638302e37ef2abb55715fed87830e60614b9
thebillkidy <thebillkidy@gmail.com>
2014-02-11 15:54:16 +0100

    PacketReader

---
+++ projects/Packet Mapper/src/main/java/be/fastrada/PacketReader.java
@@ -0,0 +1,59 @@
+package be.fastrada;
+
+public class PacketReader {
+
+    private String content;
+    private int position;
+
+    public PacketReader(String packetString) {
+        this.content = packetString.replace(" ", "");
+        this.position = 0;
+    }
+
+    public long readUint8() {
+        int byteHexLength = 2;
+        return readLong(byteHexLength);
+    }
+
+    public long readUint16() {
+        int byteHexLength = 4;
+        return readLong(byteHexLength);
+    }
+
+    public long readUint32() {
+        int byteHexLength = 8;
+        return readLong(byteHexLength);
+    }
+
+    public void resetPosition() {
+        this.position = 0;
+    }
+
+    private long readLong(int byteHexLength) {
+        long result = Long.parseLong(content.substring(position, position + byteHexLength), 16);
+        position += byteHexLength;
+        return result;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public int getId() {
+        int result = Integer.parseInt(content.substring(0, 2), 16);
+        return result;
+    }
+}
--- projects/Packet Mapper/src/test/java/be/fastrada/PacketTests.java
+++ projects/Packet Mapper/src/test/java/be/fastrada/PacketTests.java
@@ -1,103 +1,34 @@
 package be.fastrada;
 
+import org.json.simple.JSONObject;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class PacketTests {
-
     private Packet packet = new Packet("01 FF FF 0A 17 00 00 00 00");
 
     @Test
-    public void packetContent() {
-        assertEquals(packet.getContent(), "01FFFF0A1700000000");
+    public void createPacket() {
+        assertEquals("01FFFF0A1700000000", packet.getContent());
+        assertEquals("01FFFF0A1700000000", packet.getReader().getContent());
     }
 
     @Test
-    public void packetToString() {
-        assertEquals(packet.toString(), "01FFFF0A1700000000");
+    public void getId() {
+        assertEquals(1, packet.getId());
     }
 
     @Test
-    public void readByteAndReset() {
-        // Test reading a byte as test
-        checkPosition(0);
-        assertEquals(1, packet.readUint8());
-        checkPosition(2);
-
-        // Reset to 0 position
-        packet.resetPosition();
-        checkPosition(0);
+    public void getConfiguration() {
+        assertFalse(packet.getConfigFile().equals(null));
+        assertTrue(packet.getConfigFile() instanceof JSONObject);
     }
 
     @Test
-    public void readByte() {
-        checkPosition(0);
-        assertEquals(1, packet.readUint8());
-        checkPosition(2);
+    public void getStructure()  {
+        assertEquals(2, packet.getStructure().size());
     }
-
-    @Test
-    public void readMaxByte() {
-        packet.setContent("FF");
-        checkPosition(0);
-        assertEquals(255, packet.readUint8());
-        checkPosition(2);
-    }
-
-    @Test
-    public void readShort() {
-        checkPosition(0);
-        assertEquals(511, packet.readUint16()); // 01FF
-        checkPosition(4);
-    }
-
-    @Test
-    public void readMaxShort() {
-        packet.setContent("FFFF");
-        checkPosition(0);
-        assertEquals(65535, packet.readUint16());
-        checkPosition(4);
-    }
-
-    @Test
-    public void readInt() {
-        checkPosition(0);
-        assertEquals(33554186, packet.readUint32());
-        checkPosition(8);
-    }
-
-    @Test
-    public void readMaxInt() {
-        packet.setContent("FFFFFFFF");
-        checkPosition(0);
-        assertEquals(4294967295L, packet.readUint32());
-        checkPosition(8);
-    }
-
-    @Test
-    public void testByteAndShortAndInt() {
-        // Read Byte
-        readByte();
-        packet.resetPosition();
-
-        // Read Short
-        readShort();
-        packet.resetPosition();
-
-        // Read Int
-        readInt();
-        packet.resetPosition();
-    }
-
-    // Test if we reached end of the hex!
-
-    /**
-     * private methods
-     */
-
-    private void checkPosition(int expected){
-        assertEquals(expected, packet.getPosition());
-    }
-
 }
