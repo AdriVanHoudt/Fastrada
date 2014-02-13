b15f0c7a5fdabb624aff2a2527e263f1696db2e8
Mo Binni <mohamad.el-binni@student.kdg.be>
2014-02-13 17:24:14 +0100

    -removed unnecessary files -added chartjs directives for angularjs -ui improvements
    Shana & Mo

--- projects/Web/js/angular/controller/controller-navigation.js
+++ projects/Web/js/angular/controller/controller-navigation.js
@@ -4,20 +4,9 @@
 
 angular.module('fastradaApp.controllers').
     controller('NavigationCtrl', ['$scope', function ($scope) {
-        $scope.searchboxHove = function () {
-            $scope.searchIsHover = false;
-            $scope.searchBoxHover = function () {
-                $scope.searchIsHover = false;
-                return {
-                    'border-bottom': '1px rgba(151,163,75,1) solid'
-                };
-            }
-            $scope.searchIconHover = function () {
-                $scope.searchIsHover = false;
-                return {
-                    color: rgba(151, 163, 75, 1)
-                };
-            }
+        $scope.searchBoxHover = (function () {
 
-        }
+
+        });
+
     }]);
